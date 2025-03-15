4d5ef54d4e5fa597d6173f887f92a8cbcbb2f9a4
abdo-spices <spices.digital@gmail.com>
2025-03-15 06:56:19 +0200

    refactor: enhance compression configuration with additional file extensions

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,23 @@
-import { defineConfig } from 'astro/config';
-import { minify } from '@zokki/astro-minify';
-import compress from 'astro-compressor';
+import { defineConfig } from "astro/config";
+import { minify } from "@zokki/astro-minify";
+import compress from "astro-compressor";
 
 // https://astro.build/config
-export default defineConfig({	integrations: [minify({logAllFiles: false}),compress({gzip: true, brotli: false})] });
+export default defineConfig({
+	integrations: [
+		minify({ logAllFiles: false }),
+		compress({
+			gzip: true,
+			brotli: false,
+			fileExtensions: [
+				".css",
+				".js",
+				".html",
+				".xml",
+				".svg",
+				".jpg",
+				".txt",
+			],
+		}),
+	],
+});
