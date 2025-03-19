c993c8127ac92bee11eeb448ebe193ab2265379f
abdo-spices <spices.digital@gmail.com>
2025-03-19 10:24:12 +0200

    refactor: update compression settings and enable Brotli; add preload for favicon image

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,13 @@
-import { defineConfig } from "astro/config";
 import { minify } from "@zokki/astro-minify";
 import compress from "astro-compressor";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		minify({ logAllFiles: false }),
 		compress({
-			gzip: true,
-			brotli: false,
+			brotli: true,
 			fileExtensions: [
 				".css",
 				".js",
