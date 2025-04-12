489085e875ff98fafee5593375aa44910d89371b
abdo-spices <spices.digital@gmail.com>
2025-04-12 23:32:22 +0200

    feat: integrate Tailwind CSS and update index.astro structure

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,13 @@
 import { minify } from "@zokki/astro-minify";
 import compress from "astro-compressor";
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+	vite: {
+    plugins: [tailwindcss()],
+  },
 	integrations: [
 		minify({ logAllFiles: false }),
 		compress({
